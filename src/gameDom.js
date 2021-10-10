721dc0ee895e7121906c0548b9e42bfdc8fdd2d7
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-10-10 20:52:09 +0200

    :memo:  add generateGameField to gameDom file

--- src/gameDom.js
+++ src/gameDom.js
@@ -20,6 +20,10 @@ function generateGameField() {
     for (let x = 0; x < valueY; x++) {
       const cell = document.createElement("td");
       const cellText = document.createTextNode(``);
+      cell.setAttribute("class", `grass`);
+      cell.setAttribute("position", `[${y},${x}]`);
+      cell.setAttribute("value", "false");
+      cell.setAttribute("onclick", "placeBorrowers()");
 
       cell.appendChild(cellText);
       row.appendChild(cell);
@@ -44,4 +48,30 @@ function generateGameField() {
   stopButton.setAttribute("class", "main-content__stop");
 
   resetButton.setAttribute("class", "main-content__reset");
+}
+
+function placeBorrowers() {
+  window.onclick = (field) => {
+    const borrowerInfo = field.target;
+
+    if (
+      borrowerInfo.className === "grass" ||
+      borrowerInfo.className === "borrower01" ||
+      borrowerInfo.className === "borrower02" ||
+      borrowerInfo.className === "borrower03" ||
+      borrowerInfo.className === "borrower04"
+    ) {
+      let position = borrowerInfo.attributes.position.value.substr(1, 3);
+      position = position.split(",");
+      let borrowerStatus = borrowerInfo.attributes.value.value;
+
+      if (borrowerStatus) {
+        borrowerStatus = true;
+        borrowerInfo.className = "borrower01";
+      } else {
+        borrowerStatus = false;
+        borrowerInfo.className = "grass";
+      }
+    }
+  };
 }
